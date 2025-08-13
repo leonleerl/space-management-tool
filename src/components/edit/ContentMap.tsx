bb9b78a7a0318a307e424caff5491a2d8b9c0a54
leonleerl <leon.runlongli@gmail.com>
2025-08-13 15:48:53 +0800

    feat(AccFinStu): frontend display

--- src/components/edit/ContentMap.tsx
+++ src/components/edit/ContentMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AccFinStaff } from './staff'
 import { AccFinAcc } from './accommodation'
+import { AccFinStu } from './student'
 
 const ContentMap : Record<string, React.ReactNode> = {
     "16": <AccFinAcc />,
     '21': <AccFinStaff />,
+    "34": <AccFinStu />,
 }
 
 export { ContentMap }
