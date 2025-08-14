c6fdd9a81d6c9d0da9aa9c9b847134f44c1b2388
TerranConfederacy <1692017857@qq.com>
2025-08-14 08:45:44 +0800

    feat: MarketingStaff & Marketing Accommodation view are now available

--- src/components/edit/ContentMap.tsx
+++ src/components/edit/ContentMap.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { AccFinStaff } from './staff'
-import { AccFinAcc } from './accommodation'
+import { AccFinStaff, MarketingStaff } from './staff'
+import { AccFinAcc, MarketingAcc } from './accommodation'
+
 
 const ContentMap : Record<string, React.ReactNode> = {
+    "15": <MarketingAcc />,
     "16": <AccFinAcc />,
     '21': <AccFinStaff />,
+    '22': <MarketingStaff />,
 }
 
 export { ContentMap }
