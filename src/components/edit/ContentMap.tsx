12d05ff823bf1e87ab70e6403f8abc0dd5c155e5
Jwxxi <Jwxxi21@outlook.com>
2025-09-07 16:30:19 +0800

    refactor: organize ContentMap

--- src/components/edit/ContentMap.tsx
+++ src/components/edit/ContentMap.tsx
@@ -16,16 +16,21 @@ import {
 import { AccFinStu, EconomicsStu, MarketingStu } from "./student";
 
 const ContentMap: Record<string, React.ReactNode> = {
+  // Accommodation Map
   "13": <MOAcc />,
   "14": <EconomicsAcc />,
   "15": <MarketingAcc />,
   "16": <AccFinAcc />,
   "17": <DeanAcc />,
+
+  // Staff List
   "21": <AccFinStaff />,
   "22": <EconomicsStaff />,
   "23": <MarketingStaff />,
   "24": <MOStaff />,
   "25": <DeanStaff />,
+
+  // Student List
   "32": <EconomicsStu />,
   "33": <MarketingStu />,
   "34": <AccFinStu />,
