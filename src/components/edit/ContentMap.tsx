5706312e5c9e58eb65db3f98417f442077475622
Jwxxi <Jwxxi21@outlook.com>
2025-09-07 16:38:34 +0800

    feat: enhance ContentMap with new components for accommodation, student, and room management

--- src/components/edit/ContentMap.tsx
+++ src/components/edit/ContentMap.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+// Staff
 import {
   AccFinStaff,
   MarketingStaff,
@@ -6,17 +8,42 @@ import {
   EconomicsStaff,
   DeanStaff,
 } from "./staff";
+
+// Accommodation
 import {
   AccFinAcc,
   MarketingAcc,
   MOAcc,
   EconomicsAcc,
+  GroundFloorCSI,
+  GroundFloorDA,
   DeanAcc,
 } from "./accommodation";
-import { AccFinStu, EconomicsStu, MarketingStu } from "./student";
+
+// Student
+import {
+  AccFinStu,
+  EconomicsStu,
+  MarketingStu,
+  GroundFloorStu,
+  MOStu,
+} from "./student";
+
+// Room
+import {
+  DeanRoom,
+  AccFinRoom,
+  EconomicsRoom,
+  MarketingRoom,
+  MORoom,
+  GroundFloorDARoom,
+  GroundFloorCSIRoom,
+} from "./room";
 
 const ContentMap: Record<string, React.ReactNode> = {
   // Accommodation Map
+  "11": <GroundFloorCSI />,
+  "12": <GroundFloorDA />,
   "13": <MOAcc />,
   "14": <EconomicsAcc />,
   "15": <MarketingAcc />,
@@ -31,9 +58,20 @@ const ContentMap: Record<string, React.ReactNode> = {
   "25": <DeanStaff />,
 
   // Student List
+  "31": <MOStu />,
   "32": <EconomicsStu />,
   "33": <MarketingStu />,
   "34": <AccFinStu />,
+  "35": <GroundFloorStu />,
+
+  // Room Management
+  "41": <GroundFloorCSIRoom />,
+  "42": <GroundFloorDARoom />,
+  "43": <MORoom />,
+  "44": <EconomicsRoom />,
+  "45": <MarketingRoom />,
+  "46": <AccFinRoom />,
+  "47": <DeanRoom />,
 };
 
 export { ContentMap };
--- src/components/edit/accommodation/index.ts
+++ src/components/edit/accommodation/index.ts
@@ -3,3 +3,5 @@ export * from "./MarketingAcc";
 export * from "./MOAcc";
 export * from "./EconomicsAcc";
 export * from "./Dean";
+export * from "./GroundFloorCSI";
+export * from "./GroundFloorDA";
