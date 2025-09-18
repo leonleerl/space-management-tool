149bcb1f5c2f6bc380d5749d159fe5362e5580e6
Leon Lee <leon.runlongli@gmail.com>
2025-09-18 17:54:14 +0800

    fix: error

--- src/app/download/layout.tsx
+++ src/app/download/layout.tsx
@@ -2,6 +2,9 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/authOptions'
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function DownloadLayout({ children }: { children: React.ReactNode }) {   
     const session = await getServerSession(authOptions)
     if (!session) {
