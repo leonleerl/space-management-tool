41c676144e663df6cdca05ceaf01081025c3d8fa
Leon Lee <leon.runlongli@gmail.com>
2025-09-18 17:11:20 +0800

    fix: try if it can solve the problem
    Co-authored-by: TerranConfederacy <1692017857@qq.com>

--- src/app/download/layout.tsx
+++ src/app/download/layout.tsx
@@ -5,7 +5,8 @@ import { redirect } from 'next/navigation'
 export default async function DownloadLayout({ children }: { children: React.ReactNode }) {   
     const session = await getServerSession(authOptions)
     if (!session) {
-        redirect('/?error=loginRequired')
+        // redirect('/?error=loginRequired')
+        redirect('/');
     }
     return (
         <>
