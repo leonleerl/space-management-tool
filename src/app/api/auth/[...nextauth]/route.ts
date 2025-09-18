6f1c7cdf5ee9c3a54b4bcb6c33db76318e152792
Leon Lee <leon.runlongli@gmail.com>
2025-09-18 18:49:01 +0800

    revert: now it can run smoothly on localhost

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/lib/authOptions'
 
-export const runtime = 'nodejs'
-
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
